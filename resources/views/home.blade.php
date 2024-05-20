5e1c22d94486b55549e0acfe4beef5a3c885dfeb
wakhu shibero <wshibero@gmail.com>
2024-05-20 09:27:25 +0000

    added the first view. i.e the home page view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <title>The Homepage</title>
+  </head>
+  <body>
+    <div>
+      <table>
+        <tr>
+          <td><a href="/">Home</a</td>
+          <td><a href="/welcome php">Laravel docs</a></td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <p>This is the HomePage</p>
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome php', function () {
     return view('welcome');
+});
+
+Route::get('/', function(){
+    return view('home');
 });
