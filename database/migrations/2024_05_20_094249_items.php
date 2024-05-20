5a291e42cbc544024257303933052243bfc34a51
wakhu shibero <wshibero@gmail.com>
2024-05-20 09:47:52 +0000

    created the first table called items

--- database/migrations/2024_05_20_094249_items.php
+++ database/migrations/2024_05_20_094249_items.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        //
+        Schema::create('items', function(Blueprint $table){
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('activity');
+            $table->string('description');
+            $table->string('complete');
         });
     }
 
@@ -31,6 +29,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        //
+        Schema::dropIfExists('items');
     }
 };
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
 	deps = [
+   pkgs.sqlite
 		pkgs.php80
         pkgs.php80Packages.composer
 	];
