e236ac9f38986b694cdc72ccef586be05199a669
wakhu shibero <wshibero@gmail.com>
2024-05-21 13:29:47 +0000

    finished with css

--- resources/views/pages/item_view.blade.php
+++ resources/views/pages/item_view.blade.php
@@ -10,6 +10,7 @@
                <th class="data-table-items">Description</th>
                <th class="data-table-items">Completed</th>
                <th class="data-table-items"></th>
+               <th class="data-table-items"></th>
             </tr>
          </thead>
          <tbody>
@@ -18,8 +19,25 @@
                <td class="data-table-items">{{ $item->id }}</a></td>
                <td class="data-table-items">{{ $item->activity }}</td>
                <td class="data-table-items">{{ $item->description }}</td>
-               <td class="data-table-items"><a href="/item/completed/{{$item->id}}">{{ $item->complete }}</a></td>
-               <td class="data-table-items"><button><a href="/item/update form/{{ $item->id }}">Edit</a></button><button><a href="/item/delete/{{$item->id}}">Delete</a></button</td>
+               <td class="data-table-items complete-row">
+                  <a class="complete" href="/item/completed/{{$item->id}}">
+                     {{ $item->complete }}
+                  </a>
+               </td>
+               <td class="data-table-items edit-row">
+                  <button class="editbtn">
+                     <a href="/item/update form/{{ $item->id }}">
+                        Edit
+                     </a>
+                  </button>
+               </td>
+               <td class="data-table-items delete-row">
+                  <button class="deletebtn">
+                     <a href="/item/delete/{{$item->id}}">
+                        Delete
+                     </a>
+                  </button>
+               </td>
             </tr>
             @endforeach
          </tbody>
