02d818b2a0b36807ba370ad65027d6210fd927be
wakhu shibero <wshibero@gmail.com>
2024-05-21 11:57:24 +0000

    css being used first

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <div>
-   <table>
+   <table class="navbar">
      <tr>
-        <td><a href="/">Home</a</td>
-        <td><a href="/all/items">All items</a></td>
-        <td><a href="/item/create form">New Item</a></td>
-        <td><a href="/about">About</a></td>
-        <td><a href="/welcome php">Laravel docs</a></td>
+        <td id="active" class="menu-item"><a href="/">Home</a</td>
+        <td class="menu-item"><a href="/all/items">All items</a></td>
+        <td class="menu-item"><a href="/item/create form">New Item</a></td>
+        <td class="menu-item"><a href="/about">About</a></td>
+        <td class="menu-item"><a href="/welcome php">Laravel docs</a></td>
      </tr>
    </table>
 </div>
